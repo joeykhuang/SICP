b174bb7fb0bcab39608bdb37466e6f8ef4e4b4e8
josephhuxley <harrypotter0628@outlook.com>
2019-03-26 19:30:36 +0800

    test_primality

---
+++ Chapter_1/test_primality.scm
@@ -0,0 +1,27 @@
+; smallest divisor test (Θ(sqrt(n)))
+(define (smallest-divisor n) (find-divisor n 2))
+(define (find-divisor n test-divisor)
+    (cond ((> (square test-divisor) n) n)
+          ((divides? test-divisor n) test-divisor)
+          (else (find-divisor n (+ test-divisor 1)))))
+(define (divides? a b) (= (remainder b a) 0))
+
+; Fermat test (Θ(log n))
+(define (expmod base exp m)
+    (cond ((= exp 0) 1)
+          ((even? exp)
+           (remainder
+            (square (expmod base (/ exp 2) m))
+            m))
+           (else
+            (remainder
+                (* base (expmod base (- exp 1) m))
+                m))))
+(define (fermat-test n)
+    (define (try-it a)
+        (= (expmod a n n) a))
+    (try-it (+ 1 (random (- n 1)))))
+(define (fast-prime? n time)
+    (cond ((= times 0) true)
+          ((fermat-test n) (fast-prime? n (- times 1)))
+          (else false)))
