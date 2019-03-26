29ba45764b4096129984d5dcf10bb3a0654482b9
josephhuxley <harrypotter0628@outlook.com>
2019-03-26 11:21:15 +0800

    edited exercise_1.16

--- Chapter_1/exercise_1.16.scm
+++ Chapter_1/exercise_1.16.scm
@@ -1,10 +1,12 @@
 (define (expt-iter b count product)
     (if (= count 0)
-        b
+        product
         (expt-iter (* b b) (- count 1) (* product b))))
-(define (calc-expt b n)
-    (if (is-even? n)
-        (expt-iter b (/ n 2) 1)
-        (* (expt-iter b (/ (- n 1) 2) 1) b)))
+
 (define (is-even? n)
     (= (remainder n 2) 0))
+
+(define (calc-expt b n a)
+    (if (is-even? n)
+        (expt-iter b (/ n 2) a)
+        (* (expt-iter b (/ (- n 1) 2) a) b)))
