0b406b9c4437052b8adf275b4c10be403e3de3ad
josephhuxley <harrypotter0628@outlook.com>
2019-03-30 21:05:28 +0800

    exercise_1.22-25

---
+++ Chapter_1/exercise_1.24.scm
@@ -0,0 +1,38 @@
+(define (timed-prime-test n)
+    (newline)
+    (display n)
+    (start-prime-test n (runtime)))
+(define (start-prime-test n start-time)
+    (if (fermat-test n)
+        (report-prime (- (runtime) start-time))))
+(define (report-prime elapsed-time)
+    (display " *** ")
+    (display elapsed-time))
+
+(define (search-for-primes lowerbound upperbound)
+    (if (is-even? lowerbound)
+        (search-for-primes (+ lowerbound 1) upperbound)
+        (cond ((< lowerbound upperbound) (timed-prime-test lowerbound)
+            (search-for-primes (+ lowerbound 2) upperbound)))))
+
+(define (is-even? n)
+    (= (remainder n 2) 0))
+
+(define (expmod base exp m)
+    (cond ((= exp 0) 1)
+          ((even? exp)
+           (remainder
+            (square (expmod base (/ exp 2) m))
+            m))
+           (else
+            (remainder
+                (* base (expmod base (- exp 1) m))
+                m))))
+(define (fermat-test n)
+    (define (try-it a)
+        (= (expmod a n n) a))
+    (try-it (+ 1 (random (- n 1)))))
+(define (fast-prime? n time)
+    (cond ((= times 0) true)
+          ((fermat-test n) (fast-prime? n (- times 1)))
+          (else false)))
---
+++ Chapter_1/exercise_1.25.scm
@@ -0,0 +1,18 @@
+(define (expmod base exp m)
+    (remainder (fast-expt base exp) m))
+
+(define (expt-iter b count product)
+    (if (= count 0)
+        product
+        (expt-iter (* b b) (- count 1) (* product b))))
+
+(define (is-even? n)
+    (= (remainder n 2) 0))
+
+(define (calc-expt b n a)
+    (if (is-even? n)
+        (expt-iter b (/ n 2) a)
+        (* (expt-iter b (/ (- n 1) 2) a) b)))
+
+(define (fast-expt b n)
+    (calc-expt b n 1))
