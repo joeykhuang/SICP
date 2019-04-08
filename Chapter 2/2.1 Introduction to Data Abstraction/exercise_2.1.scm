c34fdb79a248c29ed089f7956e5cde492d3c3ea5
josephhuxley <harrypotter0628@outlook.com>
2019-04-08 22:09:06 +0800

    exercise_2.1

---
+++ Chapter 2/2.1 Introduction to Data Abstraction/exercise_2.1.scm
@@ -0,0 +1,6 @@
+(define (make-rat n d)
+    (let ((g (gcd n d)))
+        (cond (or (and (>= n 0) (> d 0)) (and (< n 0) <>)
+                (cons (/ n g) (/ d g))
+              (or (and (<= n 0) (< d 0)) (and (> n 0) (< d 0)))
+                (cons (/ (* -1 n) g) (/ (* -1 d) g))
