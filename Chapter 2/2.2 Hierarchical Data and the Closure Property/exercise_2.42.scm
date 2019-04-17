f3988fbf61e9a7e9b23efa987244564d70f8dec0
josephhuxley <harrypotter0628@outlook.com>
2019-04-17 09:01:06 +0800

    finished 8-queens

--- Chapter 2/2.2 Hierarchical Data and the Closure Property/exercise_2.42.scm
+++ Chapter 2/2.2 Hierarchical Data and the Closure Property/exercise_2.42.scm
@@ -1,5 +1,7 @@
+(load "Chapter 2/2.2 Hierarchical Data and the Closure Property/nested-mappings.scm")
+(load "Chapter 2/2.2 Hierarchical Data and the Closure Property/list-operations.scm")
 (define (queens board-size)
-    (define (queens-cols k)
+    (define (queen-cols k)
         (if (= k 0)
             (list empty-board)
             (filter
@@ -18,8 +20,23 @@
 
 (define empty-board '())
 
-(define (safe? column positions)
-    (not (or (filter (lambda (x)
-                        (= column x)) (map cadr positions))
-             (filter (lambda (x)
-                        (= (- ))))))
+(define (safe? col-num position)
+    (let ((this-row (car position))
+          (rest-rows (cdr position))
+          (board-size (length position)))
+        (define (safe-diagonal? position)
+            (define (safe-diagonal-iter col-number new-positions)
+                (cond ((null? new-positions) #t)
+                      ((= (abs (- (car new-positions) this-row))
+                          (abs (- col-number 1)))
+                       #f)
+                      ((> col-number board-size) #t)
+                      (else (safe-diagonal-iter (+ col-number 1) (cdr new-positions)))))
+            (safe-diagonal-iter 2 rest-rows))
+    
+        (define (safe-horizontal? position)
+            (not (member this-row rest-rows)))
+        
+        (or (= (length position) 1) 
+            (and (safe-horizontal? position)
+                 (safe-diagonal? position)))))
