353da8a7ad343cbb2fa8b704b680be4487852ff4
josephhuxley <harrypotter0628@outlook.com>
2019-04-26 07:42:22 +0800

    exercise_2.71-72

---
+++ Chapter 2/2.3 Symbolic Data/exercise_2.71.scm
@@ -0,0 +1,12 @@
+; n = 5 Huffman tree
+; (A B C D E) 31
+;      /   \
+;  E(16)  (A B C D) 15
+;            /   \
+;         D(8)  (A B C) 7
+;                  /   \
+;               C(4)  (A B) 3
+;                     /   \
+;                  B(2)   A(1)
+; 1 bit for the most frequent symbol
+; n-1 bits for the least frequent symbol
---
+++ Chapter 2/2.3 Symbolic Data/exercise_2.72.scm
@@ -0,0 +1,3 @@
+; order of growth O(n log n)
+; most frequent symbol O(n)
+; least frequent symbol O(n^2)
