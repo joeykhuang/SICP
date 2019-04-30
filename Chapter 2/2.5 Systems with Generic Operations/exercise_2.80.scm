08ad4744077de9fa468cfb422face2d5d26ddaa3
josephhuxley <harrypotter0628@outlook.com>
2019-04-30 15:56:16 +0800

    real-package representations for equ? and zero?

--- Chapter 2/2.5 Systems with Generic Operations/exercise_2.80.scm
+++ Chapter 2/2.5 Systems with Generic Operations/exercise_2.80.scm
@@ -1,23 +1,29 @@
 (define (install-scheme-number-package) 
 ;; ... 
-(put '=zero? '(scheme-number) 
-    (lambda (x) (= x 0))) 
+    (put '=zero? '(scheme-number) 
+        (lambda (x) (= x 0))) 
 'done ) 
+
+(define (install-real-package)
+;; ... 
+    (put '=zero? '(real)
+        (lambda (x) (= x 0)))
+'done )
 
 (define (install-rational-package) 
 ;; ... 
-(define (=zero? x)
-    (and (= (numer x) 0) (!= (denom x) 0))
-;; ... 
-(put '=zero? '(rational) =zero?) 
+    (define (=zero? x)
+        (and (= (numer x) 0) (!= (denom x) 0)))
+    ;; ... 
+    (put '=zero? '(rational) =zero?) 
 'done ) 
 
 (define (install-complex-package) 
 ;; ... 
-(define (=zero? x) 
-    (and (= (real-part x) 0) (= (imag-part x) 0))
-;; ... 
-(put '=zero? '(complex) =zero?) 
+    (define (=zero? x) 
+        (and (= (real-part x) 0) (= (imag-part x) 0)))
+    ;; ... 
+    (put '=zero? '(complex) =zero?) 
 'done ) 
 
 (define (=zero? x y) (apply-generic '=zero? x y)) 
