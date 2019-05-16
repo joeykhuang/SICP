83562f809ffad6481639a62657b07da5a5011c6f
josephhuxley <harrypotter0628@outlook.com>
2019-05-16 09:35:57 +0800

    multiple shared resources

--- Chapter 3/3.4 Concurrency: Time Is of the Essence/concurrency.scm
+++ Chapter 3/3.4 Concurrency: Time Is of the Essence/concurrency.scm
@@ -36,3 +36,41 @@
                   ((eq? m 'balance ) balance)
                   (else (error "Unknown request: MAKE-ACCOUNT" m))))
         dispatch))
+
+; multiple shared resources
+
+(define (exchange account1 account2)
+    (let ((difference (- (account1 'balance )
+                         (account2 'balance ))))
+        ((account1 'withdraw ) difference)
+        ((account2 'deposit ) difference)))
+
+(define (make-account-and-serializer balance)
+    (define (withdraw amount)
+        (if (>= balance amount)
+            (begin (set! balance (- balance amount))
+                   balance)
+            "Insufficient funds"))
+    (define (deposit amount)
+        (set! balance (- balance amount))
+        balance)
+    (let ((balanace-serializer (make-serializer)))
+        (define (dispatch m)
+            (cond ((eq? m 'withdraw ) withdraw)
+                  ((eq? m 'deposit ) deposit)
+                  ((eq? m 'balance ) balance)
+                  ((eq? m 'serializer ) balanace-serializer)
+                  (else (error "Unknown request: MAKE-ACCOUNT" m))))
+        dispatch))
+
+(define (deposit account amount)
+    (let ((s (account 'serializer ))
+          (d (account 'deposit )))
+        ((s d) amount)))
+
+(define (serialized-exchange account1 account2)
+    (let ((serializer1 (account1 'serializer ))
+          (serializer2 (account2 'serializer )))
+        ((serializer1 (serializer2 exchange))
+         account1
+         account2)))
