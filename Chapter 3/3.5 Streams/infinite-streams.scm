efd1bd30514e0844e4fadf50393eb550a1958039
josephhuxley <harrypotter0628@outlook.com>
2019-05-21 13:10:29 +0800

    added partial-sums

--- Chapter 3/3.5 Streams/infinite-streams.scm
+++ Chapter 3/3.5 Streams/infinite-streams.scm
@@ -27,6 +27,9 @@
 (define (add-streams s1 s2) (stream-map + s1 s2))
 (define integers
     (cons-stream 1 (add-streams ones integers)))
+(define (partial-sums s)
+    (cons-stream (stream-car s)
+                 (add-streams (stream-cdr s) (partial-sums s))))
 (define fibs
     (cons-stream
      0
