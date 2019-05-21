31f546bda2e875459c5297ec1e5173b536c220aa
josephhuxley <harrypotter0628@outlook.com>
2019-05-21 14:29:22 +0800

    stream-pairs

---
+++ Chapter 3/3.5 Streams/stream-pairs.scm
@@ -0,0 +1,20 @@
+(load "../3.5 Streams/stream-iteration.scm")
+(define (stream-append s1 s2)
+    (if (stream-null? s1)
+        s2
+        (cons-stream (stream-car s1)
+                     (stream-append (stream-cdr s1) s2))))
+
+(define (interleave s1 s2)
+    (if (stream-null? s1)
+        s2
+        (cons-stream (stream-car s1)
+                     (interleave s2 (stream-cdr s1)))))
+
+(define (pairs s t)
+    (cons-stream
+     (list (stream-car s) (stream-car t))
+        (interleave
+         (stream-map (lambda (x) (list (stream-car s) x))
+                     (stream-cdr t))
+         (pairs (stream-cdr s) (stream-cdr t)))))
