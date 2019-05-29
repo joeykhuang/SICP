195d3d59da76848549c1faf64a40558566e2f420
josephhuxley <harrypotter0628@outlook.com>
2019-05-29 13:13:42 +0800

    completed sub procedures

--- Chapter 4/4.1 The Metacircular Evaluator/eval-apply.scm
+++ Chapter 4/4.1 The Metacircular Evaluator/eval-apply.scm
@@ -1,5 +1,3 @@
-(load "Chapter 3/4.1 The Metacircular Evaluator/procedure-arguments.scm")
-
 (define (eval exp env)
     (cond ((self-evaluating? exp) exp)
           ((variable? exp) (lookup-variable-value exp env))
@@ -32,6 +30,8 @@
           (else
            (error "Unknown procedure type: APPLY" procedure))))
 
+; --------------------------------------------------------------
+
 (define (list-of-values exps env)
     (if (no-operands? exps)
         '()
@@ -61,3 +61,80 @@
                       (eval (definition value exp) env)
                       env)
     'ok )
+
+; --------------------------------------------------------------
+
+(define (self-evaluating? exp)
+    (cond ((number? exp) true)
+          ((string? exp) true)
+          (else false)))
+
+(define (variable? exp) (symbol? exp))
+
+(define (quoted? exp) (tagged-list? exp 'quote ))
+(define (text-of-quotation exp) (cadr exp))
+
+(define (tagged-list? exp tag)
+    (if (pair? exp)
+        (eq? (car exp) tag)
+        false))
+
+; --- assignment 
+(define (assignment? exp) (tagged-list? exp 'set! ))
+(define (assignment-variable exp) (cadr exp))
+(define (assignment-value exp) (caddr exp))
+
+; --- definition 
+(define (definition? exp) (tagged-list? exp 'define ))
+(define (definition-variable exp)
+    (if (symbol? (cadr exp))
+        (cadr exp)
+        (caddr exp)))
+(define (definition-value exp)
+    (if (symbol? (cadr exp))
+        (caddr exp)
+        (make-lambda (cdadr exp)        ; formal parameters
+                     (cddr exp))))      ; body
+
+(define (lambda? exp) (tagged-list? exp 'lambda ))
+(define (lambda-parameters exp) (cadr exp))
+(define (lambda-body exp) (cddr exp))
+
+(define (make-lambda parameters body)
+    (cons 'lambda (cons parameters body)))
+
+; --- conditionals
+
+(define (if? exp) (tagged-list? exp 'if ))
+(define (if-predicate exp) (cadr exp))
+(define (if-consequent exp) (caddr exp))
+(define (if-alternative exp)
+    (if (not (null? caddr exp)))
+        (cadddr exp)
+        'false )
+
+(define (make-if predicate consequent alternative)
+    (list 'if predicate consequent alternative))
+
+; --- begin
+
+(define (begin? exp) (tagged-list? exp 'begin ))
+(define (begin-actions exp) (cdr exp))
+(define (last-exp? seq) (null? (cdr seq)))
+(define (first-exp seq) (car seq))
+(define (rest-exps seq) (cdr seq))
+
+(define (sequence->exp seq)
+    (cond ((null? seq) seq)
+          ((last-exp? seq) (first-exp seq))
+          (else (make-begin seq))))
+(define (make-begin seq) (cons 'begin seq))
+
+; --- expression
+
+(define (application? exp) (pair? exp))
+(define (operator exp) (car exp))
+(define (operands exp) (cdr exp))
+(define (no-operands? ops) (null? ops))
+(define (first-operand ops) (car ops))
+(define (rest-operands ops) (cdr ops))
