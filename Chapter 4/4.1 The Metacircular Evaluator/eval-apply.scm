e354fc21d906af2f1b7ea5236c1cc83fc8dc1253
josephhuxley <harrypotter0628@outlook.com>
2019-05-29 15:45:08 +0800

    added conditionals

--- Chapter 4/4.1 The Metacircular Evaluator/eval-apply.scm
+++ Chapter 4/4.1 The Metacircular Evaluator/eval-apply.scm
@@ -116,6 +116,26 @@
 (define (make-if predicate consequent alternative)
     (list 'if predicate consequent alternative))
 
+(define (cond? exp) (tagged-list? exp 'cond ))
+(define (cond-clauses exp) (cdr exp))
+(define (cond-else-clause? clause)
+    (eq? (cond-predicate clause) 'else ))
+(define (cond-predicate clause) (car clause))
+(define (cond-actions clause) (cdr clause))
+(define (cond->if exp) (expand-clauses (cond-clauses exp)))
+(define (expand-clauses clauses)
+    (if (null? clause)
+        'false 
+        (let ((first (car clauses))
+              (rest (cdr clauses)))
+            (if (cond-else-clause? first)
+                (if (null? rest)
+                    (sequence->exp (cond-actions first))
+                    (error "ELSE clause isn't last: COND->IF" clauses))
+                (make-if (cond-predicate first)
+                         (sequence->exp (cond-actions first))
+                         (expand-clauses rest))))))
+
 ; --- begin
 
 (define (begin? exp) (tagged-list? exp 'begin ))
