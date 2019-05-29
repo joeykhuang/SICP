21f17673fb2019c6e8fa3e32a6c67b035a3a998e
josephhuxley <harrypotter0628@outlook.com>
2019-05-29 10:28:13 +0800

    added helper procedures to eval-apply

--- Chapter 4/4.1 The Metacircular Evaluator/eval-apply.scm
+++ Chapter 4/4.1 The Metacircular Evaluator/eval-apply.scm
@@ -31,3 +31,33 @@
                     (procedure-environment procedure))))
           (else
            (error "Unknown procedure type: APPLY" procedure))))
+
+(define (list-of-values exps env)
+    (if (no-operands? exps)
+        '()
+        (cons (eval (first-operands exps) env)
+              (list-of-values (rest-operands exps) env))))
+
+(define (eval-if exp env)
+    (if (true? (eval (if-predicate exp) env))
+        (eval (if-consequent exp) env)
+        (eval (if-alternate exp) env)))
+
+(define (eval-sequence exps env)
+    (cond ((last-exp? exps)
+           (eval (first-exp exps) env))
+          (else
+           (eval (first-exp exps) env)
+           (eval-sequence (rest-exps exps) env))))
+
+(define (eval-assignment exp env)
+    (set-variable-value! (assignment-variable exp)
+                         (eval (assignment-value exp) env)
+                         env)
+    'ok )
+
+(define (eval-definition exp env)
+    (define-variable! (definition-variable exp)
+                      (eval (definition value exp) env)
+                      env)
+    'ok )
